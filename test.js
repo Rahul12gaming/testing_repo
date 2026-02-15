9ffbde77c4ab617287738d2bea4b0d4eda54cdae
Rahul adhikari <99792747+Rahul12gaming@users.noreply.github.com>
2026-02-15 16:07:48 +0530

    Update test.js

--- test.js
+++ test.js
@@ -2,7 +2,6 @@ function test() {
   console.log("Hi I'm a testin function");
   console.log("Hi I'm a testin function");
   console.log("Hi I'm a testin function");
-  console.log("Hi I'm a testin function");
-}sdsdsd
+xcxcxc}sdsdsd
 
 test(); sdsd
